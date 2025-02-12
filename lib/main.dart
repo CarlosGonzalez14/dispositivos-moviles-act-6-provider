0e2c7579d5310774b1277aa6738bb883f4582bc9
Carlos Gonzalez <carlos.gonzalezba@anahuac.mx>
2025-02-12 00:46:42 -0600

    Cambios realizados para hacer calculadora

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SocialPostApp());
+  runApp(Calculadora());
 }
 
-class SocialPostApp extends StatelessWidget {
+class Calculadora extends StatelessWidget {
+  const Calculadora({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
       ),
       home: SocialPostScreen(),
     );
@@ -18,11 +20,13 @@ class SocialPostApp extends StatelessWidget {
 }
 
 class SocialPostScreen extends StatefulWidget {
+  const SocialPostScreen({super.key});
+
   @override
-  _SocialPostScreenState createState() => _SocialPostScreenState();
+  _Calculadora createState() => _Calculadora();
 }
 
-class _SocialPostScreenState extends State<SocialPostScreen> {
+class _Calculadora extends State<SocialPostScreen> {
   int likes = 233;
   bool isLiked = false;
   TextEditingController commentController = TextEditingController();
@@ -52,43 +56,13 @@ class _SocialPostScreenState extends State<SocialPostScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Publicación de TheWeeknd"),
+        title: Text("Calculadora"),
         centerTitle: true,
       ),
       body: Column(
         children: [
-          Expanded(
-            flex: 2,
-            child: Image.network(
-              "https://i.scdn.co/image/ab6761610000e5eb9e528993a2820267b97f6aae",
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                onPressed: toggleLike,
-                icon: Icon(
-                  Icons.favorite,
-                  color: isLiked ? Colors.white : Colors.red,
-                ),
-                label: Text(
-                  "$likes",
-                  style: TextStyle(color: Colors.white),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: isLiked ? Colors.red : Theme.of(context).colorScheme.inversePrimary,
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 40),
             child: Row(
               children: [
                 Expanded(
@@ -100,28 +74,106 @@ class _SocialPostScreenState extends State<SocialPostScreen> {
                     ),
                   ),
                 ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: addComment,
-                  child: Text("Enviar"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    foregroundColor: Colors.white,
-                  ),
-                ),
               ],
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: comments.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Icon(Icons.comment),
-                  title: Text(comments[index]),
-                );
-              },
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('7'),
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('8')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('9')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('/')
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('4'),
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('5')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('6')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('x')
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('1'),
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('2')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('3')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('-')
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('0'),
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('.')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('-/+')
+              ),
+              ElevatedButton(
+                onPressed: toggleLike, 
+                child: Text('+')
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: toggleLike,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                ),
+                child: Text('='),
+              ),
+            ],
           ),
         ],
       ),
